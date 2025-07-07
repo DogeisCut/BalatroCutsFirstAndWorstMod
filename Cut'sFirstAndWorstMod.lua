df240261a5410130eabab4389f1c5e1664b1edfa
DogeisCut <rickisparks06@gmail.com>
2025-07-06 23:53:00 -0600

    Add new jokers, seals, and deck backs with localizations
    Introduces new 'Cardboard' and 'Periwinkle' seals, the 'Digit Joker', and new deck backs including 'Acetate', with corresponding logic and effects. Updates asset atlases to include new graphics for jokers, enhancers, and backs. Expands English localization to cover new items and their descriptions, and refines existing text for clarity. Also adjusts the 'acetate' edition's effect and cost, and removes the 'unkempt' edition from availability.

--- Cut'sFirstAndWorstMod.lua
+++ Cut'sFirstAndWorstMod.lua
@@ -57,14 +57,19 @@ SMODS.Atlas {
 	py = 95,
 }
 
-if SMODS.Atlas then
-    SMODS.Atlas({
-        key = "modicon",
-        path = "modicon.png",
-        px = 32,
-        py = 32
-    })
-end
+SMODS.Atlas({
+    key = "modicon",
+    path = "modicon.png",
+    px = 32,
+    py = 32
+})
+
+SMODS.Atlas {
+	key = 'cfawm_jokers',
+	path = "cfawm_jokers.png",
+	px = 71,
+	py = 95,
+}
 
 -- SUITS --
 
@@ -184,9 +189,83 @@ SMODS.Seal {
 	key = 'Periwinkle',
 	atlas = 'cfawm_enhancers',
 	pos = { x = 1, y = 0 },
-	badge_colour = HEX('CCCCFF'),
+    badge_colour = HEX('CCCCFF'),
+	loc_vars = function(self, info_queue, card)
+		return { vars = { G.GAME.probabilities.normal, 5.0 } }
+	end,
 	calculate = function(self, card, context)
+		if context.main_scoring and context.cardarea == G.play then
+            local ret = {}
+			if pseudorandom('periwinkle_seal_gain') < G.GAME.probabilities.normal / 5.0 then
+				if pseudorandom('periwinkle_seal_hand_or_discard') < 1 / 2.0 then
+					ret.message = "+1 Hand!"
+                    ret.colour = G.C.BLUE
+					ease_hands_played(1)
+                else
+                    ret.message = "+1 Discard!"
+					ret.colour = G.C.RED
+					ease_discard(1)
+				end
+			end
+			return ret
+		end
+	end,
+}
 
+SMODS.Seal {
+	key = 'Cardboard',
+	atlas = 'cfawm_enhancers',
+	pos = { x = 2, y = 0 },
+    badge_colour = HEX('BB7547'),
+    loc_vars = function(self, info_queue, card)
+		return { vars = { G.GAME.probabilities.normal, 3.0 } }
+    end,
+	-- Yes this adding the cards AFTER drawing is intentional.
+	calculate = function(self, card, context)
+		if context.discard and context.other_card == card then
+            local ret = {}
+            if pseudorandom('cardboard_seal_duplicate') < G.GAME.probabilities.normal / 3.0 then
+				G.playing_card = (G.playing_card and G.playing_card + 1) or 1
+				local copy_card = copy_card(card, nil, nil, G.playing_card)
+                copy_card:add_to_deck()
+				copy_card.states.visible = nil
+                G.E_MANAGER:add_event(Event({
+					blocking = false,
+                    func = function()
+						if G.STATE ~= G.STATES.SELECTING_HAND then
+							return false
+						end
+						G.deck.config.card_limit = G.deck.config.card_limit + 1
+						table.insert(G.playing_cards, copy_card)
+						G.hand:emplace(copy_card)
+                        G.E_MANAGER:add_event(Event({
+							blocking = false,
+                            func = function()
+                                if G.STATE ~= G.STATES.SELECTING_HAND then
+                                    return false
+                                end
+								copy_card:start_materialize()
+								return true
+							end
+						}))
+						return true
+					end
+				}))
+
+				
+				ret.message = localize('k_copied_ex')
+				ret.colour = G.C.CHIPS
+				ret.func = function() 
+					G.E_MANAGER:add_event(Event({
+						func = function()
+							SMODS.calculate_context({ playing_card_added = true, cards = { copy_card } })
+							return true
+						end
+					}))
+				end
+			end
+			return ret
+		end
 	end,
 }
 
@@ -197,7 +276,7 @@ SMODS.PokerHandPart {
 	func = function(hand)
 		if #hand < 5 then return {} end
 
-		local required_ids = {6, 9, 4, 2, 15}
+		local required_ids = {6, 9, 4, 2, SMODS.Ranks['cfawm_0'].id}
 
 		local found_ids = {}
 		for _, card in ipairs(hand) do
@@ -456,38 +535,49 @@ SMODS.Shader({ key = 'unkempt', path = 'unkempt.fs' })
 SMODS.Edition {
     key = 'acetate',
     shader = 'acetate',
-    config = { },
+    config = { score_passthrough = 0.5 },
 	--disable_shadow = true,
 	disable_base_shader = true,
     in_shop = true,
     weight = 20,
-    extra_cost = 4,
+    extra_cost = 3,
     sound = { sound = "negative", per = 2, vol = 0.4 },
     loc_vars = function(self, info_queue, card)
-        return { vars = {  } }
+        return { vars = { card.edition.score_passthrough } }
     end,
     get_weight = function(self)
         return self.weight
     end,
+	calculate = function(self, card, context)
+		if (context.main_scoring and context.cardarea == G.play) or context.pre_joker then
+            local passthrough = card.edition.score_passthrough or 0
+			local mult = math.ceil(mult * passthrough)
+            local chips = math.ceil(hand_chips * passthrough)
+			return {
+                chips = chips,
+				mult = mult
+			}
+		end
+	end,
 }
 
-SMODS.Edition {
-    key = 'unkempt',
-    shader = 'unkempt',
-    config = {  },
-	--disable_shadow = true,
-	disable_base_shader = true,
-    in_shop = true,
-    weight = 20,
-    extra_cost = 4,
-    sound = { sound = "negative", per = 0.5, vol = 0.4 },
-    loc_vars = function(self, info_queue, card)
-        return { vars = {  } }
-    end,
-    get_weight = function(self)
-        return self.weight
-    end,
-}
+-- SMODS.Edition {
+--     key = 'unkempt',
+--     shader = 'unkempt',
+--     config = {  },
+-- 	--disable_shadow = true,
+-- 	disable_base_shader = true,
+--     in_shop = true,
+--     weight = 20,
+--     extra_cost = 2,
+--     sound = { sound = "negative", per = 0.5, vol = 0.4 },
+--     loc_vars = function(self, info_queue, card)
+--         return { vars = {  } }
+--     end,
+--     get_weight = function(self)
+--         return self.weight
+--     end,
+-- }
 
 -- ENHANCEMENTS --
 
@@ -498,9 +588,32 @@ SMODS.Edition {
 -- 	pos = { x = 1, y = 0 },
 -- }
 
+-- Some sort of ehancment that swaps out the base chip scoring on a card for mult.
+
 -- JOKERS --
 
--- something that does something to 0 cards
+-- x10 mult for each 0 card scored.
+
+SMODS.Joker {
+    key = "digit_joker",
+    atlas = 'cfawm_jokers',
+    pos = { x = 0, y = 0 },
+    rarity = 3,
+    blueprint_compat = true,
+    cost = 8,
+    config = { extra = { zero_x_mult = 10 }, },
+    loc_vars = function(self, info_queue, card)
+        return { vars = { card.ability.extra.zero_x_mult } }
+    end,
+    calculate = function(self, card, context)
+        if context.individual and context.cardarea == G.play and
+            context.other_card:get_id() == SMODS.Ranks['cfawm_0'].id then
+            return {
+                x_mult = card.ability.extra.zero_x_mult
+            }
+        end
+    end
+}
 
 -- DECKS/BACKS --
 
@@ -554,10 +667,11 @@ SMODS.Edition {
 SMODS.Back {
     key = "periwinkle_seal",
     pos = { x = 1, y = 0 },
-    config = {},
+    config = { hands = -1 },
     atlas = 'cfawm_backs',
     loc_vars = function(self, info_queue, back)
-        return { vars = { } }
+        --info_queue[#info_queue+1] = G.P_SEALS.cfawm_Periwinkle
+        return { vars = { self.config.hands } }
     end,
     apply = function(self, back)
         G.E_MANAGER:add_event(Event({
@@ -570,6 +684,69 @@ SMODS.Back {
         }))
     end,
 }
+
+SMODS.Back {
+    key = "cardboard_seal",
+    pos = { x = 2, y = 0 },
+    config = { discards = -1 },
+    atlas = 'cfawm_backs',
+    loc_vars = function(self, info_queue, back)
+        --info_queue[#info_queue+1] = G.P_SEALS.cfawm_Cardboard
+        return { vars = { self.config.discards } }
+    end,
+    apply = function(self, back)
+        G.E_MANAGER:add_event(Event({
+            func = function()
+                for _, card in pairs(G.playing_cards) do
+					card:set_seal('cfawm_Cardboard', true, true)
+                end
+                return true
+            end
+        }))
+    end,
+}
+
+SMODS.Back {
+    key = "acetate",
+    pos = { x = 3, y = 0 },
+    config = { discards = -1, hands = -1 },
+    atlas = 'cfawm_backs',
+    loc_vars = function(self, info_queue, back)
+        --info_queue[#info_queue+1] = G.P_CENTERS.e_cfawm_acetate
+        return { vars = { self.config.discards, self.config.hands } }
+    end,
+    apply = function(self, back)
+        G.E_MANAGER:add_event(Event({
+            func = function()
+                for _, card in pairs(G.playing_cards) do
+					card:set_edition('e_cfawm_acetate', true, true)
+                end
+                return true
+            end
+        }))
+    end,
+}
+
+-- SMODS.Back {
+--     key = "packing",
+--     pos = { x = 1, y = 0 },
+--     config = {},
+--     atlas = 'cfawm_backs',
+--     loc_vars = function(self, info_queue, back)
+--         return { vars = { } }
+--     end,
+--     apply = function(self, back)
+--         G.E_MANAGER:add_event(Event({
+--             func = function()
+--                 for _, card in pairs(G.playing_cards) do
+-- 					card:set_ability('p_standard_normal_1')
+--                 end
+--                 return true
+--             end
+--         }))
+--     end,
+-- }
+
 
 -- STICKERS --
 
--- localization/en-us.lua
+++ localization/en-us.lua
@@ -8,9 +8,28 @@ return {
             --     },
             -- },
             b_cfawm_periwinkle_seal = {
-                name = "Periwinkle Seal",
+                name = "Periwinkle Seal Deck",
                 text = {
-                    "Oops! All Periwinkle Seal",
+                    "Oops! All {T:cfawm_periwinkle_seal}Periwinkle Seal",
+                    "{C:blue}#1#{} hand",
+                    "every round",
+                },
+            },
+            b_cfawm_cardboard_seal = {
+                name = "Cardboard Seal Deck",
+                text = {
+                    "Oops! All {T:cfawm_cardboard_seal}Cardboard Seal",
+                    "{C:red}#1#{} discard",
+                    "every round",
+                },
+            },
+            b_cfawm_acetate = {
+                name = "Acetate Deck",
+                text = {
+                    "Oops! All {T:e_cfawm_acetate}Acetate",
+                    "{C:red}#1#{} discard",
+                    "{C:blue}#2#{} hand",
+                    "every round",
                 },
             }
         },
@@ -19,33 +38,52 @@ return {
             e_cfawm_acetate = {
                 name = "Acetate",
                 text = {
-                    "When scored, on top of its",
-                    "own scoring, this card scores",
-                    "{C:attention}#1#%{} of the previously",
-                    "scored card's {C:chips}chips{} and {C:mult}mult{}.",
+                    "Additionally scores {C:attention}x#1#{}",
+                    "of the current {C:chips}chips{} and {C:mult}mult{}",
+                    "{C:inactive}(rounded up)",
                 },
             },
              e_cfawm_unkempt = {
                 name = "Unkempt",
                 text = {
-                    "Doesn't do much at the moment."
+                    "Doesn't do much at the moment"
                 },
             },
         },
         Enhanced = {},
-        Joker = {},
+        Joker = {
+            j_cfawm_digit_joker = {
+                name = "Digit Joker",
+                text = {
+                    "Each played {C:attention}Zero{} gives",
+                    "{X:mult,C:white} X#1# {} Mult when scored",
+                },
+            },
+        },
         Other = {
             -- cfawm_brown_seal = {
             --     name = 'Brown Seal',
             --     text = {
             --         'Discards (with effects) the card to the',
-            --         'right when {C:attention}discarded.',
+            --         'right when {C:attention}discarded',
             --     },
             -- },
             cfawm_periwinkle_seal = {
                 name = 'Periwinkle Seal',
                 text = {
-                    "Doesn't do much at the moment."
+                    "{C:green}#1# in #2#{} chance",
+                    "to gain {C:blue}+1{} temporary hand",
+                    "or {C:red}+1{} temporary discard",
+                    "when this card is scores"
+                },
+            },
+            cfawm_cardboard_seal = {
+                name = 'Cardboard Seal',
+                text = {
+                    "When {C:attention}discarded,",
+                    "{C:green}#1# in #2#{} chance",
+                    "to add a {C:attention}copy{}",
+                    "to hand after drawing"
                 },
             },
         },
@@ -86,8 +124,9 @@ return {
         labels = {
             --cfawm_brown_seal = "Brown Seal",
             cfawm_periwinkle_seal = "Periwinkle Seal",
+            cfawm_cardboard_seal = "Cardboard Seal",
             cfawm_acetate = "Acetate",
-            e_cfawm_unkempt = "Unkempt"
+            cfawm_unkempt = "Unkempt"
         },
         poker_hand_descriptions = {
             cfawm_Funny = {
